b091c5034d599a347d225af8ef91cd07b67f0125
Holden Fromayan <fromayanh@yahoo.com>
2020-07-23 18:17:34 -0400

    Adding necessary folders

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+$('#primary-menu').on(
+    'show.zf.dropdownmenu', function() {
+      var dropdown = $(this).find('.is-dropdown-submenu');
+      dropdown.css('display', 'none');
+      dropdown.fadeIn('slow');
+  });
+  $('#primary-menu').on(
+    'hide.zf.dropdownmenu', function() {
+      var dropdown = $(this).find('.is-dropdown-submenu');
+      dropdown.css('display', 'inherit');
+      dropdown.fadeOut('slow');
+  });
+  
+  var weatherMap = new XMLHttpRequest();
+
+XPathResult.onreadystatechange = function() {};
+xhr.open('GET', 'api.openweathermap.org/data/2.5/weather?id=city.list.json.gz &appid={your api key}');
